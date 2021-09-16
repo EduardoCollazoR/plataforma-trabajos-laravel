cf0918971852d7d015a0da62f6a71000ee43d4fa
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-09-16 16:51:04 -0700

    avance

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VacanteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//Rutas de vacantes
+Route::get('/vacantes', [VacanteController::class, 'index'])->name('vacantes.index');
